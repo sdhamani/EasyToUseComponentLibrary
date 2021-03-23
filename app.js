2f21438a5d8d71988accb40890fc565418c55e04
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-03-24 02:39:35 +0530

    added carbon files and scroll functionalities

---
+++ app.js
@@ -0,0 +1,92 @@
+var btnTScrollUp = document.querySelector("#floating-button-lib");
+
+var btnScrollInstallation = document.querySelector("#installation");
+4;
+var btnScrollAvatar = document.querySelector("#avatars");
+4;
+
+var btnScrollAlerts = document.querySelector("#alerts");
+4;
+var btnScrollBadges = document.querySelector("#badges");
+4;
+var btnScrollButtons = document.querySelector("#buttons");
+4;
+var btnScrollcards = document.querySelector("#cards");
+4;
+var btnScrollImages = document.querySelector("#images");
+4;
+
+var btnScrollInputs = document.querySelector("#inputs");
+4;
+
+btnTScrollUp.addEventListener("click", function () {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollInstallation.addEventListener("click", function () {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollAvatar.addEventListener("click", function () {
+  window.scroll({
+    top: 370,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollAlerts.addEventListener("click", function () {
+  window.scroll({
+    top: 800,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollBadges.addEventListener("click", function () {
+  window.scroll({
+    top: 1480,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollButtons.addEventListener("click", function () {
+  window.scroll({
+    top: 2630,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollcards.addEventListener("click", function () {
+  window.scroll({
+    top: 3400,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollImages.addEventListener("click", function () {
+  window.scroll({
+    top: 7100,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+btnScrollInputs.addEventListener("click", function () {
+  window.scroll({
+    top: 8800,
+    left: 0,
+    behavior: "smooth",
+  });
+});
