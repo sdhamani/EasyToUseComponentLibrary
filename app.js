7692e2a6f8b8c89bcceee338ef7692f412e76fc4
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-03-26 20:18:47 +0530

    Changed styles for badge-on-icon class

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ btnTScrollUp.addEventListener("click", function () {
   });
 });
 
-btnScrollInstallation.addEventListener("click", function () {
-  console.log("Ins");
-  console.log(screenWidth);
+btnScrollInstallation.addEventListener("click", function () 
   if (screenWidth < 767) {
     var menuitem = document.getElementById("menu");
     menuitem.style.display = "none";
